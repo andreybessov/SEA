8d321ce4924e628c6622c9a44d6d74068db6a5d4
Andrey <andreybess1488@gmail.com>
2025-01-25 12:35:51 +0200

    contact-section mobile update

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
       <Initiators />
       <Communication />
       <Partners />
-      {/*<Contact />
-      <Footer /> */}
+      <Contact />
+      {/*<Footer /> */}
     </div>
   );
 }
